285f79896c863654ddd9e701cc5cddc31e912517
na2na-p <yamamoto.akyshi@gmail.com>
2022-05-02 00:03:07 +0900

    ノートの型定義拡張

--- src/misskey/note.ts
+++ src/misskey/note.ts
@@ -2,6 +2,13 @@ export type Note = {
 	id: string;
 	text: string | null;
 	reply: any | null;
+	visibility: 'public' | 'home' | 'followers' | 'specified';
+	user: {
+		id: string;
+		name: string;
+		username: string;
+		host: string | null;
+	}
 	poll?: {
 		choices: {
 			votes: number;
