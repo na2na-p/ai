2a9b33c9c5f8eb2a47ac258bfd1cd0e48fad2d43
na2na-p <na2na@na2na.dev>
2023-01-05 00:17:16 +0900

    Add DLSite reaction

--- src/utils/dlsiteParse/index.ts
+++ src/utils/dlsiteParse/index.ts
@@ -1,7 +1,15 @@
 
 import {DLSITE_CODE_PREFIX, DLSITE_URI_PREFIX, DLSITE_URI_BASE} from './CONSTANTS';
+import {JSDOM} from 'jsdom';
 
-function dlsiteParse(query: string): string[] | null {
+type workIdAndTitle = {
+	id: string,
+	title: string | null,
+}
+
+export type DlsiteParseReturnType = workIdAndTitle[] | null
+
+async function dlsiteParse(query: string): Promise<DlsiteParseReturnType> {
 	// queryの中にDLSITE_CODE_PREFIXから始まり6桁の数字が続くものがあるか
 	const obscene = query.match(new RegExp(`(${DLSITE_CODE_PREFIX.join('|')})[0-9]{6}`, 'g'));
 
@@ -13,7 +21,7 @@ function dlsiteParse(query: string): string[] | null {
 		const result: string[] = [];
 		for (const id of obscene) {
 			// 重複を排除
-			if (!obscene.includes(id)) {
+			if (!result.includes(id)) {
 				result.push(id);
 			}
 		}
@@ -21,20 +29,32 @@ function dlsiteParse(query: string): string[] | null {
 	})();
 
 	// idsのうち、実際にfetchした結果status=200だったもののみを格納するstring[]を作る
-	const result: string[] = [];
-	for (const id of ids) {
-		new Promise<void>((resolve) => {
-			const fetchUri = `${DLSITE_URI_BASE}${DLSITE_URI_PREFIX[0]}/work/=/product_id/${id}.html`;
-			fetch(fetchUri).then((response) => {
-				if (response.status === 200) {
-					result.push(id);
-				}
-				resolve();
+	const result: workIdAndTitle[] = [];
+	let loadingCounter = 0;
+
+	// TODO: リファクタしろ
+	ids.forEach(async (id) => {
+		const fetchUri = `${DLSITE_URI_BASE}${DLSITE_URI_PREFIX[0]}/work/=/product_id/${id}.html`;
+		const response = await fetch(fetchUri);
+		if (response.status === 200) {
+			const dom = new JSDOM(await response.text());
+			result.push({
+				id,
+				// DOMからtitleを取得
+				title: dom.window.document.querySelector('title')?.textContent ?? null,
 			});
+		}
+		loadingCounter++;
+	});
+
+	// TODO: リファクタしろ
+	while (loadingCounter < ids.length) {
+		await new Promise((resolve) => {
+			setTimeout(resolve, 100);
 		});
 	}
 
-	return result;
+	return result.length === 0 ? null : result;
 };
 
 export default dlsiteParse;
